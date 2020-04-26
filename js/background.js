c13dd39a6edd2179c24587528615017ef838472f
Forrest Bicker <bickerforrest@gmail.com>
2020-04-25 23:44:50 -0400

    fixed schedueling

--- js/background.js
+++ js/background.js
@@ -221,7 +221,10 @@ function getEvents() {
 }
 
 function main() {
-    setInterval(iterate(), 1000);
+    var timer = new interval(31000, function () {
+        iterate();
+    })
+    timer.run()
 }
 
 function iterate() {
@@ -251,3 +254,30 @@ load("schedule", "scheduleTable");
 newRow();
 setupTable();
 main();
+
+function interval(duration, fn) {
+    this.baseline = undefined
+
+    this.run = function () {
+        if (this.baseline === undefined) {
+            this.baseline = new Date().getTime()
+        }
+        fn()
+        var end = new Date().getTime()
+        this.baseline += duration
+
+        var nextTick = duration - (end - this.baseline)
+        if (nextTick < 0) {
+            nextTick = 0
+        }
+        (function (i) {
+            i.timer = setTimeout(function () {
+                i.run(end)
+            }, nextTick)
+        }(this))
+    }
+
+    this.stop = function () {
+        clearTimeout(this.timer)
+    }
+}
